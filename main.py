b130bf285e024d462aa60e1f681f6ecc3809c620
kolaias <63383577+kolaias@users.noreply.github.com>
2023-05-09 01:04:40 -0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, request, send_from_directory
+from werkzeug.utils import secure_filename
+from convert import docx_to_pdf
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return open('index.html').read()
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['docx_file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            output_file = os.path.splitext(filename)[0] + '.pdf'
+            output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_file)
+
+            docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), output_path)
+            return send_from_directory(app.config['UPLOAD_FOLDER'], output_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
